5e8eb34cedc300f9c5af02559694b8de9bad60ba
sushmasattuluri <VSS9@pitt.edu>
2016-02-18 17:48:28 -0500

    adding files

--- src/main/resources/public/javascript/jspracticeformvalidation.js
+++ src/main/resources/public/javascript/jspracticeformvalidation.js
@@ -1,17 +1,17 @@
 function formvalidate() {
 // Make quick references to our fields.
-var username = document.getElementById('username');
-var password = document.getElementById('password');
-var confpassword = document.getElementById('confpassword');
-var firstname = document.getElementById('firstname');
-var lastname = document.getElementById('lastname');
-var email = document.getElementById('email');
-var address = document.getElementById('address');
-var phone = document.getElementById('phone');
-var status = document.getElementById('status');
-var occupation = document.getElementById('occupation');
-var income = document.getElementById('income');
-var estassets = document.getElementById('estassets');
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var confpassword = document.getElementById("confpassword");
+var firstname = document.getElementById("firstname");
+var lastname = document.getElementById("lastname");
+var email = document.getElementById("email");
+var address = document.getElementById("address");
+var phone = document.getElementById("phone");
+var status = document.getElementById("status");
+var occupation = document.getElementById"'occupation");
+var income = document.getElementById("income");
+var estassets = document.getElementById("estassets");
 
 // Check each input in the order that it appears in the form.
 if (lengthDefine(username, 6, 8)) {
@@ -25,19 +25,21 @@ if (inputalphabet3(status, "* For your status please use alphabets only *")) {
 if (inputalphabet4(occupation, "* For your status please use alphabets only *")) {
 if (textNumeric2(income, "* Please enter numeric values *")) {
 if (textNumeric3(estassets, "* Please enter numeric values *")) {
+return true;
+}
+}
+}
+}
+}
+}
+}
+}
+}
+}
 
+return false;
 }
-}
-}
-}
-}
-}
-}
-}
-}
-}
-}
-
+return false;
 }
 // Function that checks whether input text is numeric or not.
 function textNumeric3(inputtext, alertMsg) {
@@ -45,7 +47,7 @@ var numericExpression = /^[0-9]+$/;
 if (inputtext.value.match(numericExpression)) {
 return true;
 } else {
-document.getElementById('estassets').innerText = alertMsg; // This segment displays the validation rule for zip.
+document.getElementById("estassets").innerText = alertMsg; // This segment displays the validation rule for zip.
 inputtext.focus();
 return false;
 }
@@ -56,7 +58,7 @@ var numericExpression = /^[0-9]+$/;
 if (inputtext.value.match(numericExpression)) {
 return true;
 } else {
-document.getElementById('income').innerText = alertMsg; // This segment displays the validation rule for zip.
+document.getElementById("income").innerText = alertMsg; // This segment displays the validation rule for zip.
 inputtext.focus();
 return false;
 }
@@ -67,7 +69,7 @@ var alphaExp = /^[a-zA-Z]+$/;
 if (inputtext.value.match(alphaExp)) {
 return true;
 } else {
-document.getElementById('occupation').innerText = alertMsg; // This segment displays the validation rule for name.
+document.getElementById("occupation").innerText = alertMsg; // This segment displays the validation rule for name.
 //alert(alertMsg);
 inputtext.focus();
 return false;
@@ -79,7 +81,7 @@ var alphaExp = /^[a-zA-Z]+$/;
 if (inputtext.value.match(alphaExp)) {
 return true;
 } else {
-document.getElementById('status').innerText = alertMsg; // This segment displays the validation rule for name.
+document.getElementById("status").innerText = alertMsg; // This segment displays the validation rule for name.
 //alert(alertMsg);
 inputtext.focus();
 return false;
@@ -92,7 +94,7 @@ var numericExpression = /^[0-9]+$/;
 if (inputtext.value.match(numericExpression)) {
 return true;
 } else {
-document.getElementById('phone').innerText = alertMsg; // This segment displays the validation rule for zip.
+document.getElementById("phone").innerText = alertMsg; // This segment displays the validation rule for zip.
 inputtext.focus();
 return false;
 }
@@ -104,7 +106,7 @@ var alphaExp = /^[0-9a-zA-Z]+$/;
 if (inputtext.value.match(alphaExp)) {
 return true;
 } else {
-document.getElementById('address').innerText = alertMsg; // This segment displays the validation rule for address.
+document.getElementById("address").innerText = alertMsg; // This segment displays the validation rule for address.
 inputtext.focus();
 return false;
 }
@@ -117,7 +119,7 @@ var emailExp = /^[w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/;
 if (inputtext.value.match(emailExp)) {
 return true;
 } else {
-document.getElementById('email').innerText = alertMsg; // This segment displays the validation rule for email.
+document.getElementById("email").innerText = alertMsg; // This segment displays the validation rule for email.
 inputtext.focus();
 return false;
 }
@@ -128,7 +130,7 @@ var alphaExp = /^[a-zA-Z]+$/;
 if (inputtext.value.match(alphaExp)) {
 return true;
 } else {
-document.getElementById('firstname').innerText = alertMsg; // This segment displays the validation rule for name.
+document.getElementById("firstname").innerText = alertMsg; // This segment displays the validation rule for name.
 //alert(alertMsg);
 inputtext.focus();
 return false;
@@ -140,7 +142,7 @@ var alphaExp = /^[a-zA-Z]+$/;
 if (inputtext.value.match(alphaExp)) {
 return true;
 } else {
-document.getElementById('lastname').innerText = alertMsg; // This segment displays the validation rule for name.
+document.getElementById("lastname").innerText = alertMsg; // This segment displays the validation rule for name.
 //alert(alertMsg);
 inputtext.focus();
 return false;
@@ -151,9 +153,10 @@ function  matchingpassword(inputtext,inputtext2,alertMsg) {
 if(inputtext.value == inputtext2.value){
   return true;
 } else {
-  document.getElementById('password').innerText = alertMsg; // This segment displays the validation rule for email.
-  inputtext.focus();
+  document.getElementById("confpassword").innerText = alertMsg; // This segment displays the validation rule for email.
+  inputtext2.focus();
   return false;
+ }
  }
  // Function that checks whether the input characters are restricted according to defined by user.
  function lengthDefine(inputtext, min, max) {
@@ -161,7 +164,7 @@ if(inputtext.value == inputtext2.value){
  if (uInput.length >= min && uInput.length <= max) {
  return true;
  } else {
- document.getElementById('username').innerText = "* Please enter between " + min + " and " + max + " characters *"; // This segment displays the validation rule for username
+ document.getElementById("username").innerText = "* Please enter between " + min + " and " + max + " characters *"; // This segment displays the validation rule for username
  inputtext.focus();
  return false;
  }
