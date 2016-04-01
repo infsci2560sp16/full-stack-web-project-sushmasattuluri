c4b793407587608e5a2f650190dfbe20719e857c
sushmasattuluri <VSS9@pitt.edu>
2016-03-31 20:34:08 -0400

    remove node depedencies

--- gruntfile.js
+++ gruntfile.js
@@ -74,11 +74,11 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-html-validation');
   grunt.loadNpmTasks('grunt-w3c-html-validation');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  //grunt.loadNpmTasks('grunt-contrib-qunit');
+  //grunt.loadNpmTasks('grunt-contrib-concat');
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-contrib-cssmin');
+  //grunt.loadNpmTasks('grunt-contrib-watch');
 
   // this would be run by typing "grunt test" on the command line
   grunt.registerTask('test', ['validation', 'jshint']);
