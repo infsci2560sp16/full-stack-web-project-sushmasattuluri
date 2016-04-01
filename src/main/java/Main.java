61acb77fc87f5b0fd43223ef2d7073577029cf97
sushmasattuluri <VSS9@pitt.edu>
2016-03-31 20:56:10 -0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -57,68 +57,48 @@ public class Main {
     //   }
     // }, new FreeMarkerEngine());
 
-    //POST JSON
-    // post("api/register", (req, res) -> {
-    //       Connection connection = null;
-    //       //Testing
-    //       System.out.println(req.body());
-    //     try {
-    //       connection = DatabaseUrl.extract().getConnection();
-    //       JSONObject obj = new JSONObject(req.body());
-    //       String username= obj.getString("username");
-    //       String password = obj.getString("password");
-    //       // String email = obj.getString("email");
-    //       // String fname = obj.getString("fname");
-    //       // String lname = obj.getString("lname");
-    //       // String gender = obj.getString("gender");
-    //       // String language = obj.getString("language");
-    //       // String planguage = obj.getString("planguage");
-    //       // String topic = obj.getString("topic");
-    //
-    //       String sql = "INSERT INTO users (username,password) VALUES ('"+ username + "','" + password + "')";
-    //
-    //       connection = DatabaseUrl.extract().getConnection();
-    //       Statement stmt = connection.createStatement();
-    //       stmt.executeUpdate(sql);
-    //
-    //       // ResultSet rs = stmt.executeQuery("SELECT * FROM users where username ='" + username + "'");
-    //       // Map<String, Object> currentuser = new HashMap<>();
-    //       //
-		// 			// currentuser.put("username", rs.getString("username"));
-		// 			// currentuser.put("email", rs.getString("email"));
-    //       //
-    //       // return currentuser;
-    //      return req.body();
-    //     } catch (Exception e) {
-    //       return e.getMessage();
-    //     } finally {
-    //       if (connection != null) try{connection.close();} catch(SQLException e){}
-    //     }
-    //   });
+  //  POST JSON
+    post("api/register", (req, res) -> {
+          Connection connection = null;
+          //Testing
+          System.out.println(req.body());
+        try {
+          connection = DatabaseUrl.extract().getConnection();
+          JSONObject obj = new JSONObject(req.body());
+          String username= obj.getString("username");
+          String password = obj.getString("password");
+          // String email = obj.getString("email");
+          // String fname = obj.getString("fname");
+          // String lname = obj.getString("lname");
+          // String gender = obj.getString("gender");
+          // String language = obj.getString("language");
+          // String planguage = obj.getString("planguage");
+          // String topic = obj.getString("topic");
 
+          String sql = "INSERT INTO users (username,password) VALUES ('"+ username + "','" + password + "')";
 
-  /*  get("/", (request, response) -> {
-/*
-     Map<String, Object> attributes = new HashMap<>();
-        attributes.put("message", "Hello World!");
+          connection = DatabaseUrl.extract().getConnection();
+          Statement stmt = connection.createStatement();
+          stmt.executeUpdate(sql);
+
+          // ResultSet rs = stmt.executeQuery("SELECT * FROM users where username ='" + username + "'");
+          // Map<String, Object> currentuser = new HashMap<>();
+          //
+					// currentuser.put("username", rs.getString("username"));
+					// currentuser.put("email", rs.getString("email"));
+          //
+          // return currentuser;
+         return req.body();
+        } catch (Exception e) {
+          return e.getMessage();
+        } finally {
+          if (connection != null) try{connection.close();} catch(SQLException e){}
+        }
+      });
 
 
 
-            /*     ArrayList<String> users_list = new ArrayList<String>();
-                 users_list.add("tenthousand");
-                users_list.add("twentythousand");
-
-
-                Map<String, Object> attributes = new HashMap<>();
-                 attributes.put("messages", users_list);
-
-
-
-                  return new ModelAndView(attributes, "users.ftl");
-               }, new FreeMarkerEngine());
-
-*/
-get("/", (req, res) -> {
+get("/users", (req, res) -> {
              ArrayList<String> users = new ArrayList<String>();
              users.add("John Doe");
              users.add("Tony Doe");
