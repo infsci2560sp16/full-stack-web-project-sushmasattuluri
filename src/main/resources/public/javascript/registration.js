3fafe79482a47615200b1bc0d59362957ac5f883
sushmasattuluri <VSS9@pitt.edu>
2016-04-02 15:31:21 -0400

    test

--- src/main/resources/public/javascript/registration.js
+++ src/main/resources/public/javascript/registration.js
@@ -1,34 +1,29 @@
 window.onload = init;
 
-$(function(){
-        $("#submit").click(function(){
+function submitTest(){
+  alert("Submit test....!");
+		return true;
+}
 
-        var username = $("#username").val();
-				var password = $("#password").val();
-        // var email = $("#email").val();
-        // var fname = $("#fname").val();
-        // var lname =$("#lname").val();
-				// var gender =$('input:radio[name="gender"]:checked').val();
-				// var language =$("#language").val();
-				// var planguage =$("#planguage").val();
-				// var topic =$("#topic").val();
+  $function() {
+       $("#submit").click(function(){
+         alert("Welcome! You have registered successfully.. not really!");
+              var username = $("#username").val();
+              var password = $("#password").val();
+              var obj = JSON.stringify({"username":username,"password":password});
 
-        var obj = JSON.stringify({"username":username,"password":password
-																	// "email":email,"fname":fname,"lname":lname,
-																	// "gender":gender,"language":language,"planguage":planguage,"topic":topic
-                                });
               $.ajax({
-                  contentType:'application/json',
-                  url: '/api/register',
-                  type: "POST",
-                  datatype: "json",
-                  data: obj,
-                  success: function(data) {
-                      alert("Welcome! You have registered successfully!");
-                      window.location.href='/index.html';
-											console.log(data);
-                  }
-              });
-                    return false;
-          });
-   });
+             		url : "/api/register",
+             		type : "POST",
+                contentType:'application/json',
+             		dataType: "json",
+             		data : obj,
+            		success : function(data) {
+                  alert("Welcome! You have registered successfully!");
+                  window.location.href='/index.html'
+                  console.log(data);
+                }
+          	});
+            return false;
+        });
+  });
