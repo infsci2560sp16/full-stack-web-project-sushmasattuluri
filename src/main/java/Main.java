79183a492c7d82560c49f061368cc0665714e1e6
sushmasattuluri <VSS9@pitt.edu>
2016-04-02 17:33:48 -0400

    test1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -83,7 +83,44 @@ public class Main {
                        }
                    });
 
+       //POST JSON
+                       post("api/register", (req, res) -> {
+                             Connection connection = null;
+                             //Testing
+                             System.out.println(req.body());
+                           try {
+                             connection = DatabaseUrl.extract().getConnection();
+                             JSONObject obj = new JSONObject(req.body());
+                             String username = obj.getString("username");
+                             String password = obj.getString("password");
+                             String email = obj.getString("email");
+                             String fname = obj.getString("fname");
+                             String lname = obj.getString("lname");
+                             String gender = obj.getString("gender");
+                             String language = obj.getString("language");
+                             String planguage = obj.getString("planguage");
+                             String topic = obj.getString("topic");
 
+                             String sql = "INSERT INTO users VALUES ('"+ username + "','" + password + "','" + email + "','" + fname + "','"+ lname + "','" + gender + "','" + language + "','" + planguage + "','" + topic + "')";
+
+                             connection = DatabaseUrl.extract().getConnection();
+                             Statement stmt = connection.createStatement();
+                             stmt.executeUpdate(sql);
+
+                             ResultSet rs = stmt.executeQuery("SELECT * FROM users where username ='" + username + "'");
+                             Map<String, Object> currentuser = new HashMap<>();
+
+                   					currentuser.put("username", rs.getString("username"));
+                   					currentuser.put("email", rs.getString("email"));
+
+                             return currentuser;
+                           //  return req.body();
+                           } catch (Exception e) {
+                             return e.getMessage();
+                           } finally {
+                             if (connection != null) try{connection.close();} catch(SQLException e){}
+                           }
+                         });
 
     //get("/hello", (req, res) -> "Hello World");
 
@@ -120,46 +157,45 @@ public class Main {
     //   }
     // }, new FreeMarkerEngine());
 
-  //  POST JSON
-    post("/api/register", (req, res) -> {
-          Connection connection = null;
-          //Testing
-          System.out.println(req.body());
-        try {
-          connection = DatabaseUrl.extract().getConnection();
-          JSONObject obj = new JSONObject(req.body());
-          //String username= "sushma";
-          // String password ="password";
-         String username= obj.getString("username");
-        String password = obj.getString("password");
-          // String email = obj.getString("email");
-          // String fname = obj.getString("fname");
-          // String lname = obj.getString("lname");
-          // String gender = obj.getString("gender");
-          // String language = obj.getString("language");
-          // String planguage = obj.getString("planguage");
-          // String topic = obj.getString("topic");
-
-          String sql = "INSERT INTO users (username,password) VALUES ('"+ username + "','" + password + "')";
-
-          connection = DatabaseUrl.extract().getConnection();
-          Statement stmt = connection.createStatement();
-          stmt.executeUpdate(sql);
-
-           ResultSet rs = stmt.executeQuery("SELECT * FROM users where username ='" + username + "'");
-          Map<String, Object> currentuser = new HashMap<>();
-
-				 	 currentuser.put("username", rs.getString("username"));
-				// 	// currentuser.put("email", rs.getString("email"));
-
-            return currentuser;
-        //return req.body();
-        } catch (Exception e) {
-          return e.getMessage();
-        } finally {
-          if (connection != null) try{connection.close();} catch(SQLException e){}
-        }
-      });
+  // //  POST JSON
+  //   post("api/register", (req, res) -> {
+  //         Connection connection = null;
+  //         //Testing
+  //         System.out.println(req.body());
+  //       try {
+  //         connection = DatabaseUrl.extract().getConnection();
+  //         JSONObject obj = new JSONObject(req.body());
+  //
+  //        String username= obj.getString("username");
+  //        String password = obj.getString("password");
+  //         String email = obj.getString("email");
+  //         String fname = obj.getString("fname");
+  //         String lname = obj.getString("lname");
+  //         String gender = obj.getString("gender");
+  //         String language = obj.getString("language");
+  //         String planguage = obj.getString("planguage");
+  //         String topic = obj.getString("topic");
+  //
+  //         String sql = "INSERT INTO users (username,password) VALUES ('"+ username + "','" + password + "')";
+  //
+  //         connection = DatabaseUrl.extract().getConnection();
+  //         Statement stmt = connection.createStatement();
+  //         stmt.executeUpdate(sql);
+  //
+  //          ResultSet rs = stmt.executeQuery("SELECT * FROM users where username ='" + username + "'");
+  //         Map<String, Object> currentuser = new HashMap<>();
+  //
+	// 			 	 currentuser.put("username", rs.getString("username"));
+	// 			// 	// currentuser.put("email", rs.getString("email"));
+  //
+  //           return currentuser;
+  //       //return req.body();
+  //       } catch (Exception e) {
+  //         return e.getMessage();
+  //       } finally {
+  //         if (connection != null) try{connection.close();} catch(SQLException e){}
+  //       }
+  //     });
 
 
 
