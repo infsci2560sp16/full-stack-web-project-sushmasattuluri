63405b9b8343ede34da2f0e48e66b024d41d1392
sushmasattuluri <VSS9@pitt.edu>
2016-04-02 23:08:07 -0400

    j2

--- src/main/resources/public/javascript/registration.js
+++ src/main/resources/public/javascript/registration.js
@@ -7,7 +7,7 @@ function register(){
 	var confpassword = $("#confpassword").val();
 	var email = $("#email").val();
   alert("XXXXXXXsuccessfully accepted"+ firstname + "  " + lastname+ "  " + username+ "  " + password + "  "+ email);
-	$.post("/register",{"firstname":firstname,"lastname":lastname,"username":username,"password":password,"confpassword":confirmPassword,"email":email},function(data){
+	$.post("/register",{"firstname":firstname,"lastname":lastname,"username":username,"password":password,"confpassword":confirmPassword,"email":email},function(){
 	alert("successfully accepted");
 	window.location.href="/myIndex?username="+JSON.parse(data).username;
   });
