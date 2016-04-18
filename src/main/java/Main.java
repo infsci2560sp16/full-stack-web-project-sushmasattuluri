a8d7f9992180c198fe39c17e99efcffbd746feb4
sushmasattuluri <VSS9@pitt.edu>
2016-04-18 14:19:08 -0400

    ftl_json_xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -102,6 +102,7 @@ public class Main {
                          return data;
                      }, gson::toJson);
 
+
          post("/api/about", (req, res) -> {
                        Map<String, Object> data = new HashMap<>();
                        data.put("content1", "Grow your savings to cover 3 to 6 months of unplanned expenses in a conservatively invested portfolio.40%stocks and 60%bonds");
@@ -224,6 +225,31 @@ public class Main {
   //       }
   //     });
 
+  get("/db", (req, res) -> {
+      Connection connection = null;
+      Map<String, Object> attributes = new HashMap<>();
+      try {
+        connection = DatabaseUrl.extract().getConnection();
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+        ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
+
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
+    }, new FreeMarkerEngine());
 
 
    //
